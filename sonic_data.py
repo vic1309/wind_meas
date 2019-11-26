839519ac7a278f5d180a1ec80e9e55bf97fa2bf1
Astrid Myren <astrid@hipphurra.com>
2019-11-26 13:48:16 +0100

    Started on reading csv file

--- sonic_data.py
+++ sonic_data.py
@@ -2,3 +2,13 @@
 This is the file for the sonic data.
 """
 
+__author__ = "Astrid Myren, UiB"
+__email__ = "astrid.myren@student.uib.no"
+
+# Import pandas
+import pandas as pd
+
+
+# Load csv
+df = pd.read_csv("Sonic_processed_2019_11_04.csv")
+print(df)
