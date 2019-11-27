917c487749ed76d5b9d774e39b30e06732cdb99f
Astrid Myren <astrid@hipphurra.com>
2019-11-27 20:58:19 +0100

    Trying to get an overview

--- sonic_data.py
+++ sonic_data.py
@@ -7,8 +7,21 @@ __email__ = "astrid.myren@student.uib.no"
 
 # Import pandas
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Load csv
-df = pd.read_csv("Sonic_processed_2019_11_04.csv")
-print(df)
+df = pd.read_csv("Sonic_processed_2019_11_04.csv", low_memory=False)
+#print(df)
+
+# Output the number of rows
+print("Total rows: {0}".format(len(df)))
+
+# See which headers are available
+print(list(df))
+df.head(4)
+
+pd.isnull(df).sum()
+# plt.plot([1, 2, 3, 4])
+# plt.ylabel('some numbers')
+# plt.show(
