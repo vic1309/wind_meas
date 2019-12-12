9ad07b1ead09012a81cf2a1c5ffc283881784fe4
Astrid Myren <astrid@hipphurra.com>
2019-12-12 14:39:16 +0100

    Can save files to csv

--- sonic_data.py
+++ sonic_data.py
@@ -7,8 +7,22 @@ __email__ = "astrid.myren@student.uib.no"
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdate
 import numpy as np
+import os
+
+
+def load_csv(file):
+    dataframe = pd.read_csv(file, na_filter=False, low_memory=False)
+    dataframe['datetime'] = pd.to_datetime(dataframe["time (seconds since 1970-01-01 00:00:00)"], unit='s', origin='unix')
+    dataframe = dataframe.set_index('datetime')
+    dataframe = dataframe.drop(columns=["time (seconds since 1970-01-01 00:00:00)"])
+    return dataframe
+
+
+def resample_df(dataframe):
+    # Resample
+    dataframe = dataframe.resample('T').mean()
+    return dataframe
 
 
 def simple_wind_from_vector(u, v):
@@ -51,53 +65,44 @@ def simple_wind_from_vector(u, v):
     return wd
 
 
-# Load csv
-df = pd.read_csv("Sonic_processed_2019_11_05.csv", na_filter=False, low_memory=False)
-df['datetime'] = pd.to_datetime(df["time (seconds since 1970-01-01 00:00:00)"], unit='s', origin='unix')
-df = df.set_index('datetime')
-df = df.drop(columns=["time (seconds since 1970-01-01 00:00:00)"])
+def uh_wd(dataframe):
+    # Get horizontal wind speed and wind direction
+    dataframe['Horizontal wind speed'] = dataframe.apply(lambda row: np.sqrt(row['X Wind Speed (m/s)']**2+row['Y Wind Speed (m/s)']**2), axis=1)
+    dataframe['Wind direction'] = dataframe.apply(lambda row: simple_wind_from_vector(row['X Wind Speed (m/s)'], row['Y Wind Speed (m/s)']), axis=1)
+    return dataframe
 
 
-# Resample
-df = df.resample('T').mean()
+def plot_wind(dataframe):
+
+    ax = plt.gca()
+    dataframe['X Wind Speed (m/s)'].plot(kind='line', ax=ax)
+    dataframe['Y Wind Speed (m/s)'].plot(kind='line', ax=ax)
+    dataframe['Z Wind Speed (m/s)'].plot(kind='line', ax=ax)
+    plt.title('Wind speed from sonic anemometer')
+    plt.ylabel('Wind speed [m/s]')
+    plt.legend(loc='best')
+    # plt.savefig('SA_wind.png')
+    plt.show()
+
+    plt.subplots(2, 1, figsize=(15,20)) # 3x3 frame
+    plt.subplot(211)
+    plt.plot(dataframe['Horizontal wind speed'])
+    plt.title('Horizontal wind speed [m/s]')
+    plt.ylabel('Wind speed [m/s]')
+
+    plt.subplot(212)
+    plt.plot(dataframe['Wind direction'])
+    plt.title('Wind direction')
+    plt.ylabel('Degrees')
+    plt.show()
 
 
-# Get horizontal wind speed and wind direction
-df['Horizontal wind speed'] = df.apply(lambda row: np.sqrt(row['X Wind Speed (m/s)']**2+row['Y Wind Speed (m/s)']**2), axis=1)
-df['Wind direction'] = df.apply(lambda row: simple_wind_from_vector(row['X Wind Speed (m/s)'], row['Y Wind Speed (m/s)']), axis=1)
-
-
-ax = plt.gca()
-df['X Wind Speed (m/s)'].plot(kind='line', ax=ax)
-df['Y Wind Speed (m/s)'].plot(kind='line', ax=ax)
-df['Z Wind Speed (m/s)'].plot(kind='line', ax=ax)
-plt.title('Wind speed from sonic anemometer')
-plt.ylabel('Wind speed [m/s]')
-plt.legend(loc='best')
-# plt.savefig('SA_wind.png')
-plt.show()
-
-
-fig, axs = plt.subplots(2, 1, figsize=(15,20)) # 3x3 frame
-
-plt.subplot(211)
-plt.plot(df['Horizontal wind speed'])
-plt.title('Horizontal wind speed [m/s]')
-plt.ylabel('Wind speed [m/s]')
-
-plt.subplot(212)
-plt.plot(df['Wind direction'])
-plt.title('Wind direction')
-plt.ylabel('Degrees')
-
-plt.show()
 # cols = ['Horizontal wind speed', 'Wind direction']
 # axes = df[cols].plot(subplots=True, figsize=(11,9))
 # for ax in axes:
 #     ax.set_ylabel('Wind speed (m/s)')
 # plt.savefig('SA_uh_wd.png')
 # plt.show()
-
 
 # print(df)
 # Output the number of rows
@@ -106,3 +111,12 @@ plt.show()
 # print(list(df))
 # print(df.head())
 # print(pd.isnull(df).sum())
+
+directory = r'C:\Users\astri\.PyCharm2019.2\wind_meas\Sonic_processed'
+for entry in os.scandir(directory):
+    df = load_csv(entry)
+    # print(df.dtypes)
+    df = resample_df(df)
+    df = uh_wd(df)
+    filename = 'SP' + os.path.splitext(os.path.basename(entry))[0][-10:] + '.csv'
+    df.to_csv(os.path.join(directory, 'SP' + os.path.splitext(os.path.basename(entry))[0][-10:] + '.csv'))
