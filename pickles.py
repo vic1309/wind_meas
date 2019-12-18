d8fb45db3cb9b712515f252d6a65608e04170495
Astrid Myren <astrid@hipphurra.com>
2019-12-18 22:17:40 +0100

    New file for pickled data

---
+++ pickles.py
@@ -0,0 +1,40 @@
+"""
+This is the file for the sonic data.
+"""
+
+__author__ = "Astrid Myren, UiB"
+__email__ = "astrid.myren@student.uib.no"
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+sonic = r'C:\Users\astri\.PyCharm2019.2\wind_meas\pickled_data\sonic_mer_res.pkl'
+station = r'C:\Users\astri\.PyCharm2019.2\wind_meas\pickled_data\wsd.pkl'
+s_df = pd.read_pickle(sonic)
+w_df = pd.read_pickle(station)
+
+s_df['datetime'] = pd.to_datetime(s_df["time"], unit='s',
+                                       origin='unix')
+s_df = s_df.set_index('datetime')
+s_df = s_df.drop(columns=["time"])
+
+w_df['datetime'] = pd.to_datetime(w_df["time"], unit='s',
+                                       origin='unix')
+w_df = w_df.set_index('datetime')
+w_df = w_df.drop(columns=["time"])
+
+s_df['Uh'] = s_df.apply(
+        lambda row: np.sqrt(row['x_vel'] ** 2 + row['y_vel'] ** 2), axis=1)
+
+ax = plt.gca()
+s_df['Uh'].plot(kind='line', ax=ax)
+w_df['wspd'].plot(kind='line', ax=ax)
+plt.legend(loc='best')
+plt.show()
+
+ax1 = plt.gca()
+w_df['wspd'].hist(ax=ax1)
+plt.legend(loc='best')
+plt.show()
